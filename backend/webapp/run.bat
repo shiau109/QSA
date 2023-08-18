c95f162ce59e5be89ab02a5bacc2b6f4899b9c3d
shiau109 <shiau109@gmail.com>
2023-08-18 14:39:24 +0800

    Update run.bat

--- backend/webapp/run.bat
+++ backend/webapp/run.bat
@@ -8,8 +8,9 @@ ECHO WELCOME TO QAS
 SET ENVNAME=FAST
 call C:\ProgramData\Anaconda3\Scripts\activate.bat %ENVNAME%
 cd C:\Users\shiau\CAS\backend\webapp
-uvicorn main:app --reload
+uvicorn main:app --reload --port 7999 
 
 
 
 
+ 
